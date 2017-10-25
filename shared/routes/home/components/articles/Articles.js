c87432677b2d4d235a6af4740cb20343f470cde8
Magnús Magnússon <m.magnusson@gmail.com>
2017-10-25 23:00:11 +0000

    refactor

--- shared/routes/home/components/articles/Articles.js
+++ shared/routes/home/components/articles/Articles.js
@@ -19,45 +19,47 @@ export default class Articles extends Component {
 
     return (
       <div className={s.articles}>
-        <div className={s.articles__row}>
-          <div className={s.articles__header}>
-            <h2 className={s.articles__headerTitle}>From our lovely people</h2>
-            <h2 className={s.articles__headerTitle}>or almost</h2>
-          </div>
+        <div className={s.articles__container}>          
+          <div className={s.articles__row}>
+            <div className={s.articles__header}>
+              <h2 className={s.articles__headerTitle}>From our lovely people</h2>
+              <h2 className={s.articles__headerTitle}>or almost</h2>
+            </div>
 
-          {articles && (
-            <ul className={s.articles__list}>
-              {articles.slice(0, show).map((article, i) => {
-                const { uid, data } = article.article;
+            {articles && (
+              <ul className={s.articles__list}>
+                {articles.slice(0, show).map((article, i) => {
+                  const { uid, data } = article.article;
 
-                if (!uid) {
-                  return null;
-                }
+                  if (!uid) {
+                    return null;
+                  }
 
-                const url = `/articles/${uid}`;
-                const title = getField(data.title, 'title');
-                const description = getField(data.short_description, 'title');
-                const date = data.publication_date;
+                  const url = `/articles/${uid}`;
+                  const title = getField(data.title, 'title');
+                  const description = getField(data.short_description, 'title');
+                  const date = data.publication_date;
 
-                return (
-                  <li
-                    className={s.articles__item}
-                    key={`article-${i}`} // eslint-disable-line
-                  >
-                    <Link className={s.articles__link} to={url}>
-                      <p className={s.articles__date}>{format(date, 'DD MMMM')}</p>
+                  return (
+                    <li
+                      className={s.articles__item}
+                      key={`article-${i}`} // eslint-disable-line
+                      >
+                        <Link className={s.articles__link} to={url}>
+                          <p className={s.articles__date}>{format(date, 'DD MMMM')}</p>
 
-                      <div className={s.articles__inner}>
-                        <h2 className={s.articles__title}>{title}</h2>
-                        <p className={s.articles__description}>{description}</p>
-                        <span className={s.articles__button}>Read more</span>
-                      </div>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          )}
+                          <div className={s.articles__inner}>
+                            <h2 className={s.articles__title}>{title}</h2>
+                            <p className={s.articles__description}>{description}</p>
+                            <span className={s.articles__button}>Read more</span>
+                          </div>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
         </div>
       </div>
     );
