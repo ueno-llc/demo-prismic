f2328108af9e155a3242a1231551159efcc90c64
Magnús Magnússon <m.magnusson@gmail.com>
2017-10-25 16:19:58 +0000

    Article list and heading mixins

--- shared/routes/articles/article-list/ArticleList.js
+++ shared/routes/articles/article-list/ArticleList.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { inject } from 'mobx-react';
 import { withJob } from 'react-jobs';
+import { getField } from 'utils/prismic';
 
 import Heading from 'components/heading';
 
@@ -19,36 +20,50 @@ class Articles extends PureComponent {
     const { jobResult: articles } = this.props;
 
     return (
-      <div className={s.articleList__container}>
-        <Helmet title="Articles" />
-        <Heading>Articles</Heading>
+      <div className={s.articleList}>
+        <div className={s.articleList__container}>
+          <Helmet title="Articles" />
 
-        {articles && (
-          <ul>
-            {articles.map((article) => {
-              const { uid } = article;
+          {articles && (
+            <ul className={s.articleList__list}>
+              {articles.map((article, i) => {
+                const { uid, data } = article;
 
-              if (!uid) {
-                return null;
-              }
+                if (!uid) {
+                  return null;
+                }
 
-              const url = `/articles/${uid}`;
-              const title = article.data.title[0].text;
-              const published = new Date(article.first_publication_date);
+                const url = `/articles/${uid}`;
+                const title = article.data.title[0].text;
+                const description = getField(data.short_description, 'title');
+                const published = new Date(article.first_publication_date);
 
-              return (
-                <li key={uid}>
-                  <Link to={url}>{title}</Link>,
-                  published {published.toUTCString()}
-                </li>
-              );
-            })}
-          </ul>
-        )}
+                return (
+                  <li className={s.articleList__item} key={uid}>
+                    <Link to={url} className={s.articleList__block}>
+                      <div className={s.articelList__top}>
+                        <div className={s.articleList__image}>
+                          <img src={require(`assets/images/${i+1}.jpg`)} alt=""/>
+                        </div>
+                      </div>
+                      <div className={s.articleList__middle}>
+                        <div className={s.articleList__title}>{title}</div>
+                        <div className={s.articleList__description}>{description}</div>
+                      </div>
+                      <div className={s.articleList__bottom}>
+                        <div className={s.articleList__link}>Read more</div>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
 
-        {!articles && (
-          <p>No articles at the moment.</p>
-        )}
+          {!articles && (
+            <p>No articles at the moment.</p>
+          )}
+        </div>
       </div>
     );
   }
