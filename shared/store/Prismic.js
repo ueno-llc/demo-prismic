95ae7232e746d0ed3f4415fbce42218eac354794
Hrafn Einarsson <hrafn.einarsson@gmail.com>
2017-10-26 11:06:06 +0000

    search api route

--- shared/store/Prismic.js
+++ shared/store/Prismic.js
@@ -1,5 +1,7 @@
 import { extendObservable } from 'mobx';
 import timing from 'utils/timing';
+import groupBy from 'lodash/groupBy';
+import { getField, linkResolver } from 'utils/prismic';
 import config from '../../config';
 
 const localApiUrl = config('localApiUrl');
@@ -79,6 +81,32 @@ export default class Prismic {
       .catch((err) => {
         console.warn('Error fetching prismic data', err);
         return [];
+      });
+  }
+
+  @timing.promise
+  search(q) {
+    const url = `${apiUrl}/prismic/search/${q}`;
+
+    return this.fetch(url)
+      .then(data => data.results)
+      .then(results => results.map(m => ({ // map it to search results
+        id: m.id,
+        title: getField(m.data.title, 'title'),
+        description: m.type === 'article'
+          ? getField(m.data.short_description, 'text')
+          : getField(m.data.description_seo, 'text'),
+        to: linkResolver(m),
+        type: m.type,
+        isPage: m.tags.some(s => s === 'page') || m.type === 'custom_page',
+      })))
+      .then(results => ({ // split into categories
+        ...groupBy(results, m => m.isPage ? 'pages' : m.type),
+        count: results.length,
+      }))
+      .catch((err) => {
+        console.warn('Error fetching prismic data', err);
+        return { count: 0 };
       });
   }
 }
