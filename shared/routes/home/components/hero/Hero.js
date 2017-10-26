f9c823f94e35ca82c621600a245be52f0f51d328
Jérémy Barbet <jeremgraph@gmail.com>
2017-10-26 14:46:59 +0000

    Move colors to prismic

--- shared/routes/home/components/hero/Hero.js
+++ shared/routes/home/components/hero/Hero.js
@@ -2,14 +2,11 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { TimelineLite } from 'gsap';
 import { TransitionGroup } from 'react-transition-group';
-import { random } from 'lodash';
 
 import { getField } from 'utils/prismic';
 
 import Content from './Content';
 import s from './Hero.scss';
-
-const colors = ['#fe5830', '#c948b4', '#93cfe3', '#ffce5a', '#6dd9d1'];
 
 export default class Hero extends PureComponent {
   static propTypes = {
@@ -20,12 +17,12 @@ export default class Hero extends PureComponent {
     current: 0,
   }
 
-  changeSlide = (index) => {
+  changeSlide = (index, color) => {
     const t = new TimelineLite();
     const ease = 'Power4.easeInOut';
 
     t.to(this.el, 0.6, {
-      backgroundColor: colors[random(colors.length - 1)],
+      backgroundColor: color,
       ease,
     });
 
@@ -56,7 +53,7 @@ export default class Hero extends PureComponent {
           <ul className={s.hero__pagination}>
             {carousel && carousel.map((_, i) => (
               <li // eslint-disable-line
-                onClick={() => this.changeSlide(i)}
+                onClick={() => this.changeSlide(i, getField(carousel[i].color))}
                 className={s(s.hero__item, { active: current === i })}
                 key={`pagination-item-${i}`} // eslint-disable-line
               />
